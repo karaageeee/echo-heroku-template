b702817838f8d57e86302a58918fa5e288973c4e
Yusei Takao <yuseing335@gmail.com>
2020-07-22 16:09:38 +0900

    update auth middleware

--- conf/auth.go
+++ conf/auth.go
@@ -1,42 +1,6 @@
 package conf
 
-import (
-	"os"
-)
-
 const (
 	// AuthTokenKey is used for API request header
 	AuthTokenKey string = "API-TOKEN"
 )
-
-// AuthUser defines API user
-type AuthUser struct {
-	Name  string
-	Token string
-}
-
-// AuthUsers defines slice of user
-type AuthUsers []AuthUser
-
-var authUsersInstance AuthUsers
-
-// Init singleton instance of AuthUsers
-func init() {
-	env := os.Getenv("ENV")
-	if env == "production" {
-		authUsersInstance = AuthUsers{
-			AuthUser{Name: "WebAppUser", Token: "TBD"},
-			AuthUser{Name: "SampleAppUser", Token: "TBD"},
-		}
-	} else {
-		authUsersInstance = AuthUsers{
-			AuthUser{Name: "WebAppUser", Token: "web-app-user-token"},
-			AuthUser{Name: "SampleAppUser", Token: "sample-user-token"},
-		}
-	}
-}
-
-// GetAuthUsers returns singleton instance
-func GetAuthUsers() AuthUsers {
-	return authUsersInstance
-}
--- middleware/auth.go
+++ middleware/auth.go
@@ -5,6 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 // AuthValidate is to check http request.
@@ -12,7 +13,7 @@ func AuthValidate() echo.MiddlewareFunc {
 	return middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
 		AuthScheme: appConf.AuthTokenKey,
 		Validator: func(key string, c echo.Context) (bool, error) {
-			for _, authUser := range appConf.GetAuthUsers() {
+			for _, authUser := range getAuthUsers() {
 				if authUser.Token == key {
 					log.WithFields(log.Fields{"User": authUser.Name}).Info("Request accepted")
 					return true, nil
@@ -22,4 +23,27 @@ func AuthValidate() echo.MiddlewareFunc {
 			return false, nil
 		},
 	})
+}
+
+// AuthUser defines API user
+type AuthUser struct {
+	Name  string
+	Token string
+}
+
+// AuthUsers defines slice of user
+type AuthUsers []AuthUser
+
+var authUsersInstance AuthUsers
+
+// getAuthUsers returns singleton instance
+func getAuthUsers() AuthUsers {
+
+	if authUsersInstance == nil {
+		authUsersInstance = AuthUsers{
+			AuthUser{Name: "WebAppUser", Token: os.Getenv("WEB_APP_USER_API_TOKEN")},
+			// AuthUser{Name: "NewUser", Token: "xxxx"},
+		}
+	}
+	return authUsersInstance
 }
